7bc0a9ddc9e537eb025d94489e42e946da44b333
StefHancock76 <stefshubert76@gmail.com>
2023-07-15 21:12:22 -0500

    Updating for class

---
+++ jsday2.js
@@ -0,0 +1,45 @@
+let budget = 20;
+
+if(budget > 50){
+    console.log("Ride a taxi");
+} else if (budget > 30 && budget <= 50){
+    console.log("Ride a bus");
+} else if (budget > 10 && budget <= 30){
+    console.log("Ride a train");
+} else {
+    console.log("Ride a bike");
+}
+
+
+//from class today 07-12-2023
+//let day1 = "Sunday"
+//let day2 = "Monday"
+//let day3 = "thrusday"
+//function checkWeekend(day) {
+//if (day.toLocaleLowerCase() == "sunday" || day.toLocaleLowerCase() == "saturday"){
+//console.log("It is the Weekend")
+//} else {
+//console.log("It is not the weekend");
+//}
+//}
+//checkWeekend(day1);
+//checkWeekend(day2);
+//checkWeekend(day3);
+//checkWeekend("SatUrDay")
+
+
+
+//let students = ["John", "Kyle", "Tom", "Steve", "Mildred"]
+//let students2 = {
+//stu1: "Haoyu",
+//stu2:"Leo",
+//}
+//for (let item in students){
+//console.log(item)
+//}
+//for (let item of students){
+//console.log(item)
+//}
+//for (let item in students2){
+//console.log(students2[item]);
+//}
