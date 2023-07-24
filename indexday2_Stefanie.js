62a7d158f8f8bdc6edb2b3e0617867c0eb40c480
StefHancock76 <stefshubert76@gmail.com>
2023-07-24 11:16:19 -0500

    Working class assingment 2.2.

--- indexday2_Stefanie.js
+++ indexday2_Stefanie.js
@@ -8,7 +8,6 @@ for(q1test=1;q1test<=100;q1test++){
   }
 }
 
-//Add code here
 
 //Q2. Using an if-else statement, determin is a year is a leap year or not.
 //Research what are the conditions to be considered a leap year. "To be a leap year, the year number must be divisible by four – except for end-of-century years, which must be divisible by 400. This means that the year 2000 was a leap year, although 1900 was not. 2024, 2028, 2032 and 2036 are all leap years."
